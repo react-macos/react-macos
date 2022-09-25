71c788f71d74687d2a64d34bcf0d1109d8d01260
张森 <zsdota2@sina.com>
2022-09-25 12:19:38 +0800

    feat: 添加dock栏

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,13 +2,48 @@ import { Outlet } from 'umi'
 import StatusBar from './widgets/StatusBar'
 import Dock from './widgets/Dock'
 import styles from './index.less'
+import { MacDockItem } from '../interface/dockInterface'
 
 export default function DesktopLayout() {
+  const dockList: MacDockItem[] = [
+    {
+      label: 'launchpad',
+      icon: require('./images/dock-icon/launchpad.png'),
+    },
+    {
+      label: 'bear',
+      icon: require('./images/dock-icon/bear.png'),
+    },
+    {
+      label: 'facetime',
+      icon: require('./images/dock-icon/facetime.png'),
+    },
+    {
+      label: 'github',
+      icon: require('./images/dock-icon/github.png'),
+    },
+    {
+      label: 'mail',
+      icon: require('./images/dock-icon/mail.png'),
+    },
+    {
+      label: 'safari',
+      icon: require('./images/dock-icon/safari.png'),
+    },
+    {
+      label: 'terminal',
+      icon: require('./images/dock-icon/terminal.png'),
+    },
+    {
+      label: 'vscode',
+      icon: require('./images/dock-icon/vscode.png'),
+    },
+  ]
   return (
     <div className={styles.desktopWrapper}>
       <StatusBar />
       <Outlet />
-      <Dock />
+      <Dock items={dockList} />
     </div>
   )
 }
--- src/layouts/widgets/Dock.tsx
+++ src/layouts/widgets/Dock.tsx
@@ -1,8 +1,126 @@
 import React from 'react'
 import styles from './Dock.less'
+import classNames from 'classnames'
+import { MacDockItem, MacDockProps } from '../../interface/dockInterface'
 
-const Dock: React.FC = () => {
-  return <div className={styles.dockWrapper}></div>
+const Dock: React.FC<{ props: MacDockProps }> = (props: any) => {
+  const {
+    className,
+    style,
+    placement = 'bottom',
+    size = 'default',
+    items,
+    renderItem: customRenderItem,
+    onClick,
+  } = props
+
+  const [currentIndex, setCurrentIndex] = React.useState<number>()
+
+  // dock中没一个app大小样式
+  const sizeStyle: React.CSSProperties =
+    typeof size === 'number'
+      ? {
+          width: size,
+          height: size,
+        }
+      : {}
+
+  /**
+   * 处理鼠标进入事件
+   * @param index
+   */
+  const handleMouseEnter = (index: number): void => {
+    setCurrentIndex(index)
+  }
+
+  /**
+   * 处理鼠标离开事件
+   * @returns
+   */
+  const handleMouseLeave = (): void => {
+    setCurrentIndex(undefined)
+  }
+
+  /**
+   * 渲染每一项 dock的每一个app
+   * @param item
+   * @param index
+   * @returns
+   */
+  const renderItem = (item: MacDockItem, index: number): React.ReactNode => {
+    const liClassName: string = classNames(
+      styles['mac-dock-item'],
+      { [styles['mac-dock-item-small']]: size === 'small' },
+      currentIndex !== undefined
+        ? {
+            [styles['mac-dock-item-prev-second']]: currentIndex - 2 === index,
+            [styles['mac-dock-item-prev']]: currentIndex - 1 === index,
+            [styles['mac-dock-item-current']]: currentIndex === index,
+            [styles['mac-dock-item-next']]: currentIndex + 1 === index,
+            [styles['mac-dock-item-next-second']]: currentIndex + 2 === index,
+          }
+        : null
+    )
+    const contentNode: React.ReactNode = <img src={item.icon} />
+
+    return (
+      <li
+        className={liClassName}
+        style={sizeStyle}
+        key={index}
+        title={item.label}
+        onMouseEnter={() => handleMouseEnter(index)}
+        onClick={() => onClick?.(item)}
+      >
+        {customRenderItem?.(item, contentNode) || contentNode}
+      </li>
+    )
+  }
+
+  /**
+   * 渲染所有项  app的集合
+   * @param items
+   * @returns
+   */
+  const renderItems = (items: MacDockItem[]): React.ReactNode => {
+    return items.map((item, index) => {
+      return renderItem(item, index)
+    })
+  }
+
+  /**
+   * 渲染程序坞
+   * @returns
+   */
+  const renderDock = (): React.ReactNode => {
+    if (!Array.isArray(items)) {
+      return
+    }
+    return (
+      <ul className={styles['mac-dock']} onMouseLeave={handleMouseLeave}>
+        {renderItems(items)}
+      </ul>
+    )
+  }
+  return (
+    <>
+      <div
+        className={classNames(
+          styles['mac-dock-container'],
+          styles[`mac-dock-placement-${placement}`],
+          className
+        )}
+        style={style}
+      >
+        {renderDock()}
+      </div>
+      <div className={styles.icpLink}>
+        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">
+          京 ICP 备 2022024018号
+        </a>
+      </div>
+    </>
+  )
 }
 
 export default Dock
