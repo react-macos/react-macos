83500402052277ab77f4292dd6dead386a036586
Cheng Kuan <chengkuan@starcross.tech>
2022-09-28 18:56:47 +0800

    feat: 状态栏简单布局

--- src/layouts/widgets/StatusBar.tsx
+++ src/layouts/widgets/StatusBar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import apple from '@/assets/svgs/apple.svg'
 import styles from './StatusBar.less'
 
 const StatusBar: React.FC = () => {
-  return <div className={styles.statusBarWrapper}></div>
+  return (
+    <div className={styles.statusBarWrapper}>
+      <div className={styles.statusLeft}>
+        <div className={styles.statusItem}>
+          <img src={apple} alt="apple-logo" />
+        </div>
+      </div>
+      <div className={styles.statusRight}>
+        <div className={styles.statusItem}>
+          9月28日 周三 18:53
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default StatusBar
